a21e7458951a1b0c834c8b1f2272d09de0f7107b
Edwin Yoner <edwinyoner@gmail.com>
2024-10-27 16:21:11 -0500

    Noveno commit: Go modules

--- 02-Nivel-Intermedio/04-Paquetes-y-Modulos/calculadora/calculadora.go
+++ 02-Nivel-Intermedio/04-Paquetes-y-Modulos/calculadora/calculadora.go
@@ -1,7 +1,7 @@
 /*
 Archivo: crear_paquete.go
 Autor: Edwin Yoner
-Fecha: 26/10/2024
+Fecha: 27/10/2024
 Versión: 1.0
 
 Este archivo muestra cómo crear un paquete personalizado en Go.
@@ -12,10 +12,10 @@ package calculadora
 
 // Sumar devuelve la suma de dos números enteros.
 func Sumar(a, b int) int {
-    return a + b
+	return a + b
 }
 
 // Restar devuelve la resta de dos números enteros.
 func Restar(a, b int) int {
-    return a - b
+	return a - b
 }
